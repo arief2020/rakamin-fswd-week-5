81b6c2bf0056c2eee3bf657a8f896ddd50cdc0fe
arief2020 <msyaifullahalarief@gmail.com>
2024-02-07 18:32:25 +0700

    update new design web

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+
+const navRegistration = document.getElementById("nav-registration")
+const navList = document.getElementById("nav-list-data")
+const registrationContent = document.getElementById("registration-content")
+const listContent = document.getElementById("list-data-content")
 const people = [];
 const functionSubmit = (event) => {
   event.preventDefault();
+  const message = []
   const names = document.getElementById("input-name").value;
   const age = document.getElementById("input-age").value;
   const pocketMoney = document.getElementById("input-pocket").value;
+  if (names.length < 10) {
+    message.push(`name field must have 10 character or more`)
+    // return console.log(`name field must have 10 character or more`);
+  }
+  if (age < 25) {
+    message.push(`minimum age must be 25`)
+  }
+  if (pocketMoney < 100000 || pocketMoney > 1000000) {
+    message.push("minimum money must be 100000 and maximum money must be 1000000")
+  }
+  if (message) {
+    console.log(message)
+    return
+  }
   let person = {
     names: names,
     age: age,
@@ -11,10 +31,10 @@ const functionSubmit = (event) => {
   };
   people.push(person);
   console.log(people);
-  const textResume = resumePeople()
-  const resumeTag = document.getElementById("content-resume")
-  resumeTag.innerHTML = textResume
-  readPeople()
+  const textResume = resumePeople();
+  const resumeTag = document.getElementById("content-resume");
+  resumeTag.innerHTML = textResume;
+  readPeople();
   clearInput();
 };
 
@@ -33,20 +53,27 @@ const readPeople = () => {
     <td>${people[index].age}</td>
     <td>${people[index].pocketMoney}</td>
     `;
-    document.getElementById("table-budy").appendChild(data)
+    document.getElementById("table-budy").appendChild(data);
   }
-  
 };
 const resumePeople = () => {
-    
-    let averageOfAge = 0
-    let averageOfMoney = 0
-    for (let index = 0; index < people.length; index++) {
-        averageOfAge += parseInt(people[index].age) 
-        averageOfMoney += parseInt(people[index].pocketMoney)
-    }
-    averageOfAge = averageOfAge / people.length
-    averageOfMoney = averageOfMoney / people.length
+  let averageOfAge = 0;
+  let averageOfMoney = 0;
+  for (let index = 0; index < people.length; index++) {
+    averageOfAge += parseInt(people[index].age);
+    averageOfMoney += parseInt(people[index].pocketMoney);
+  }
+  averageOfAge = averageOfAge / people.length;
+  averageOfMoney = averageOfMoney / people.length;
 
-    return `Rata rata pendaftar memiliki uang sangu sebesar ${averageOfMoney} dengan rata rata umur ${averageOfAge}`
-}
+  return `Rata rata pendaftar memiliki uang sangu sebesar ${averageOfMoney} dengan rata rata umur ${averageOfAge}`;
+};
+
+navRegistration.addEventListener('click', ()=>{
+  registrationContent.classList.add("active")
+  listContent.classList.remove("active")
+})
+navList.addEventListener('click', ()=>{
+  listContent.classList.add("active")
+  registrationContent.classList.remove("active")
+})
